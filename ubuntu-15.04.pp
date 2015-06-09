ae5ebe553ab044666dadc55bc5b39b2704ccf8f3
FiveFilters.org <fivefilters@fivefilters.org>
2015-06-09 13:43:44 +0200

    http extension

--- ubuntu-15.04.pp
+++ ubuntu-15.04.pp
@@ -75,6 +75,9 @@ class php {
 	package { "php5-gd": ensure => latest }
 	package { "php5-imagick": ensure => latest }
 	package { "php5-json": ensure => latest }
+	package { "php-http": ensure => latest }
+	package { "php5-raphf": ensure => latest }
+	package { "php5-propro": ensure => latest }
 	file { "/etc/php5/mods-available/fivefilters-php.ini":
 		ensure => present,
 		content => "engine = On
